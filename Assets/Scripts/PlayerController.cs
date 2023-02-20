6a4146034868abb94b0e4176b07a67742a9b3428
salomaotech <salomao@taimber.com>
2023-02-20 12:44:24 -0300

    Adicionado pulo ao personagem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,17 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody rg;
 
+    Collider coll;
+
+    bool pressedjump = false;
 
     // Start is called before the first frame update
     void Start()
     {
 
         rg = GetComponent<Rigidbody>();
+
+        coll = GetComponent<Collider>();
 
     }
 
@@ -59,13 +64,52 @@ public class PlayerController : MonoBehaviour
 
         float jAxis = Input.GetAxis("Jump");
 
+        bool isGrounded = CheckGrounded();
+
         if(jAxis > 0f)
         {
 
-            Vector3 jumpVector = new Vector3(0f, jumpSpeed, 0f);
-            rg.velocity = rg.velocity + jumpVector;
+            if(!pressedjump && isGrounded)
+            {
+
+                pressedjump = true;
+
+                Vector3 jumpVector = new Vector3(0f, jumpSpeed, 0f);
+                rg.velocity = rg.velocity + jumpVector;
+
+            }
+            else
+            {
+
+
+                pressedjump = false;
+
+            }
+
 
         }
+
+       
+    }
+
+    bool CheckGrounded()
+    {
+
+        float sizeX = coll.bounds.size.x;
+        float sizeZ = coll.bounds.size.z;
+        float sizeY = coll.bounds.size.y;
+
+        Vector3 corner1 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
+        Vector3 corner2 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
+        Vector3 corner3 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
+        Vector3 corner4 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
+
+        bool grounded1 = Physics.Raycast(corner1, new Vector3(0, -1, 0), 0.01f);
+        bool grounded2 = Physics.Raycast(corner2, new Vector3(0, -1, 0), 0.01f);
+        bool grounded3 = Physics.Raycast(corner3, new Vector3(0, -1, 0), 0.01f);
+        bool grounded4 = Physics.Raycast(corner4, new Vector3(0, -1, 0), 0.01f);
+
+        return (grounded1 || grounded2 || grounded3 || grounded4);
 
     }
 
